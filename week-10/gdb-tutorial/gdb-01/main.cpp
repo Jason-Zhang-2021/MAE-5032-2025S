a6f6ebcbb08adc1a21e04293775e982258e79d71
Ju Liu <liujuy@gmail.com>
2025-04-21 19:35:44 +0800

    in

--- week-10/gdb-tutorial/gdb-01/main.cpp
+++ week-10/gdb-tutorial/gdb-01/main.cpp
@@ -2,12 +2,14 @@
 
 int main()
 {
-    int j = 3;
-    int k = 7;
-    j += k;
-    k = j * 2;
-    printf("Hello there \n");
-    return 0;
+  int j = 3;
+  int k = 7;
+  j += k;
+  k = j * 2;
+  int * l = new int [5];
+  l[4] = 100.0;
+  printf("l[4] is $d \n", l[4]);
+  return 0;
 }
 
 // EOF
