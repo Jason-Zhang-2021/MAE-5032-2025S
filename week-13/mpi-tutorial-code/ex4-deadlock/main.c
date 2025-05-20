477d3720511efaad2ae8e7615dfaff2948c6ce7f
Ju Liu <liujuy@gmail.com>
2025-05-20 08:15:43 +0800

    in

--- week-13/mpi-tutorial-code/ex4-deadlock/main.c
+++ week-13/mpi-tutorial-code/ex4-deadlock/main.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include <mpi.h>
 
@@ -10,21 +11,31 @@ int main(int argc, char **argv)
 
   int tag1 = 42, tag2 = 43;
   int sendMessage, recvMessage;
+  
+  int bufsize = 2 * (MPI_BSEND_OVERHEAD + sizeof(int));
+  void *buffer = malloc(bufsize);
+  MPI_Buffer_attach(buffer, bufsize);
+  
   if(rank == 0)
   {
     sendMessage = 7;
-    MPI_Ssend(&sendMessage, 1, MPI_INT, 1, tag1, MPI_COMM_WORLD);
+    MPI_Bsend(&sendMessage, 1, MPI_INT, 1, tag1, MPI_COMM_WORLD);
     MPI_Recv(&recvMessage, 1, MPI_INT, 1, tag2, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
   }
   else if(rank == 1)
   {
     sendMessage = 14;
+    MPI_Bsend(&sendMessage, 1, MPI_INT, 0, tag2, MPI_COMM_WORLD);
     MPI_Recv(&recvMessage, 1, MPI_INT, 0, tag1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-    MPI_Ssend(&sendMessage, 1, MPI_INT, 0, tag2, MPI_COMM_WORLD);
   }
 
   printf("Rank %d received the following integer: %d\n", rank, recvMessage);
   
+  void *detach_buffer;
+  int detach_size;
+  MPI_Buffer_detach(&detach_buffer, &detach_size);
+  free(detach_buffer);
+
   MPI_Finalize();
   return 0;
 }
