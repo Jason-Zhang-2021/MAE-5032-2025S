9e48874d1256313fc0cd4053327f81b6d8c41e83
Ju Liu <liujuy@gmail.com>
2025-06-03 11:04:58 +0800

    in

--- week-14/petsc-tutorial/ex2-vec-viewer/ex2.c
+++ week-14/petsc-tutorial/ex2-vec-viewer/ex2.c
@@ -8,31 +8,30 @@ static char help[] = "Parallel vector layout.\n\n";
 
 int main(int argc,char **argv)
 {
-  PetscErrorCode ierr;
   PetscMPIInt    rank;
   PetscInt       i,istart,iend,n = 6,nlocal;
   PetscScalar    v,*array;
   Vec            x;
 
-  ierr = PetscInitialize(&argc,&argv,(char*)0,help);if (ierr) return ierr;
-  ierr = MPI_Comm_rank(PETSC_COMM_WORLD,&rank);CHKERRQ(ierr);
+  PetscInitialize(&argc,&argv,(char*)0,help);
+  MPI_Comm_rank(PETSC_COMM_WORLD,&rank);
 
   // enable passing -n as a command line argument
-  ierr = PetscOptionsGetInt(NULL,NULL,"-n",&n,NULL);CHKERRQ(ierr);
+  PetscOptionsGetInt(NULL,NULL,"-n",&n,NULL);
 
   // Create a vector, specifying only its global dimension.
   // When using VecCreate(), VecSetSizes() and VecSetFromOptions(),
   // the vector format (currently parallel or sequential) is
   // determined at runtime.  Also, the parallel partitioning of
   // the vector is determined by PETSc at runtime.
-  ierr = VecCreate(PETSC_COMM_WORLD,&x);CHKERRQ(ierr);
-  ierr = VecSetSizes(x,PETSC_DECIDE,n);CHKERRQ(ierr);
-  ierr = VecSetFromOptions(x);CHKERRQ(ierr);
+  VecCreate(PETSC_COMM_WORLD,&x);
+  VecSetSizes(x,PETSC_DECIDE,n);
+  VecSetFromOptions(x);
 
   // PETSc parallel vectors are partitioned by
   // contiguous chunks of rows across the processors.  Determine
   // which vector are locally owned.
-  ierr = VecGetOwnershipRange(x,&istart,&iend);CHKERRQ(ierr);
+  VecGetOwnershipRange(x,&istart,&iend);
 
   PetscPrintf(PETSC_COMM_SELF, "rank [%d]: istart = %d iend = %d \n", rank, istart, iend );
 
@@ -46,7 +45,7 @@ int main(int argc,char **argv)
     for (i=0; i<n; i++) 
     {
       v    = (PetscReal)(i);
-      ierr = VecSetValues(x,1,&i,&v,INSERT_VALUES);CHKERRQ(ierr);
+      VecSetValues(x,1,&i,&v,INSERT_VALUES);
     }
   }
 
@@ -54,23 +53,23 @@ int main(int argc,char **argv)
   // VecAssemblyBegin(), VecAssemblyEnd()
   // Computations can be done while messages are in transition
   // by placing code between these two statements.
-  ierr = VecAssemblyBegin(x);CHKERRQ(ierr);
-  ierr = VecAssemblyEnd(x);CHKERRQ(ierr);
+  VecAssemblyBegin(x);
+  VecAssemblyEnd(x);
 
   // View the vector
-  ierr = VecView(x,PETSC_VIEWER_STDOUT_WORLD);CHKERRQ(ierr);
+  VecView(x,PETSC_VIEWER_STDOUT_WORLD);
 
   // Access the vector values directly. Each processor has access only
   // to its portion of the vector. For default PETSc vectors VecGetArray()
   // does NOT involve a copy
-  ierr = VecGetLocalSize(x,&nlocal);CHKERRQ(ierr);
+  VecGetLocalSize(x,&nlocal);
   PetscPrintf(PETSC_COMM_SELF, "rank [%d]: nlocal = %d \n", rank, nlocal );
-  ierr = VecGetArray(x,&array);CHKERRQ(ierr);
+  VecGetArray(x,&array);
   for (i=0; i<nlocal; i++) array[i] = rank + 1;
-  ierr = VecRestoreArray(x,&array);CHKERRQ(ierr);
+  VecRestoreArray(x,&array);
 
   // View the vector
-  ierr = VecView(x, PETSC_VIEWER_STDOUT_WORLD);CHKERRQ(ierr);
+  VecView(x, PETSC_VIEWER_STDOUT_WORLD);
 
   // Viewer to output in HDF5 format
   PetscViewer pv;
@@ -81,10 +80,10 @@ int main(int argc,char **argv)
 
   // Free work space.  All PETSc objects should be destroyed when they
   // are no longer needed.
-  ierr = VecDestroy(&x);CHKERRQ(ierr);
+  VecDestroy(&x);
 
-  ierr = PetscFinalize();
-  return ierr;
+  PetscFinalize();
+  return 0;
 }
 
 // EOF
