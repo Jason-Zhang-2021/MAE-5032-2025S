d5b48d4d20b5558a2f2941548c56931dd49b1240
Ju Liu <liujuy@gmail.com>
2025-06-03 15:29:11 +0800

    in

--- week-14/petsc-tutorial/ex4-ksp/run_script.sh
+++ week-14/petsc-tutorial/ex4-ksp/run_script.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-~/lib/petsc-3.22.2-debug/bin/mpiexec -np 5 ./ex4.out \
+~/lib/petsc-3.22.2-debug/bin/mpiexec -np 2 ./ex4.out \
   -n 10000 \
   -ksp_type gmres \
   -ksp_gmres_restart 30 \
   -ksp_rtol 1.0e-10 \
   -ksp_atol 1.0e-50 \
   -ksp_max_it 1500 \
-  -pc_type none \
+  -pc_type hypre \
   -ksp_monitor_short \
   -ksp_converged_reason \
-  -ksp_view
-
+  -ksp_view \
+  -log_view
 
