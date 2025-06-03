f532f97fd7cb00e1ae6533fd28c19bc635b71846
Ju Liu <liujuy@gmail.com>
2025-06-03 11:28:57 +0800

    in

--- week-14/petsc-tutorial/ex3-mat/ex3.c
+++ week-14/petsc-tutorial/ex3-mat/ex3.c
@@ -8,39 +8,38 @@ int main(int argc,char **argv)
   MPI_Comm        comm;
   PetscMPIInt     rank;
   PetscInt        n=5,m=5,*dnnz,*onnz,i,rstart,rend,M,N;
-  PetscErrorCode  ierr;
 
-  ierr = PetscInitialize(&argc,&argv,0,help);if (ierr) return ierr;
+  PetscInitialize(&argc,&argv,0,help);
   comm = MPI_COMM_WORLD;
   MPI_Comm_rank(comm, &rank);
-  ierr = PetscMalloc2(m,&dnnz,m,&onnz);CHKERRQ(ierr);
+  PetscMalloc2(m,&dnnz,m,&onnz);
   for (i=0; i<m; i++) 
   {
     dnnz[i] = 1;
     onnz[i] = 1;
   }
-  ierr = MatCreateAIJ(comm,m,n,PETSC_DETERMINE,PETSC_DETERMINE,PETSC_DECIDE,dnnz,PETSC_DECIDE,onnz,&A);CHKERRQ(ierr);
-  ierr = MatSetFromOptions(A);CHKERRQ(ierr);
-  ierr = MatSetUp(A);CHKERRQ(ierr);
-  ierr = PetscFree2(dnnz,onnz);CHKERRQ(ierr);
+  MatCreateAIJ(comm,m,n,PETSC_DETERMINE,PETSC_DETERMINE,PETSC_DECIDE,dnnz,PETSC_DECIDE,onnz,&A);
+  MatSetFromOptions(A);
+  MatSetUp(A);
+  PetscFree2(dnnz,onnz);
 
-  ierr = MatSetOption(A,MAT_NEW_NONZERO_ALLOCATION_ERR,PETSC_TRUE);CHKERRQ(ierr);
-  ierr = MatGetOwnershipRange(A,&rstart,&rend);CHKERRQ(ierr);
-  ierr = MatGetSize(A,&M,&N);CHKERRQ(ierr);
-  ierr = PetscPrintf(comm, "Matrix size is %d by %d \n", M, N);
-  ierr = PetscPrintf(PETSC_COMM_SELF, "rank [%d] rstart = %d , rend = %d \n", rank, rstart, rend);
+  MatSetOption(A,MAT_NEW_NONZERO_ALLOCATION_ERR,PETSC_TRUE);
+  MatGetOwnershipRange(A,&rstart,&rend);
+  MatGetSize(A,&M,&N);
+  PetscPrintf(comm, "Matrix size is %d by %d \n", M, N);
+  PetscPrintf(PETSC_COMM_SELF, "rank [%d] rstart = %d , rend = %d \n", rank, rstart, rend);
   for (i=rstart; i<rend; i++) 
   {
-    ierr = MatSetValue(A,i,i,2.0,INSERT_VALUES);CHKERRQ(ierr);
+    MatSetValue(A,i,i,2.0,INSERT_VALUES);
     if (rend<N) 
-      ierr = MatSetValue(A,i,rend,1.0,INSERT_VALUES);CHKERRQ(ierr);
+      MatSetValue(A,i,rend,1.0,INSERT_VALUES);
   }
-  ierr = MatAssemblyBegin(A,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
-  ierr = MatAssemblyEnd(A,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
-  ierr = MatView(A,PETSC_VIEWER_STDOUT_WORLD);CHKERRQ(ierr);
-  ierr = MatDestroy(&A);CHKERRQ(ierr);
-  ierr = PetscFinalize();
-  return ierr;
+  MatAssemblyBegin(A,MAT_FINAL_ASSEMBLY);
+  MatAssemblyEnd(A,MAT_FINAL_ASSEMBLY);
+  MatView(A,PETSC_VIEWER_STDOUT_WORLD);
+  MatDestroy(&A);
+  PetscFinalize();
+  return 0;
 }
 
 // EOF
